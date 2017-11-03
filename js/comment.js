4d599d0cadb1a29e527ce98745186ac396c66c43
Anastasiia <penkovaanastasiia@gmail.com>
2017-11-03 14:05:49 +0200

    First commit

---
+++ js/comment.js
@@ -0,0 +1,45 @@
+window.onload = function(){
+         document.getElementById('add').onclick = startComment;
+    }
+
+    document.onkeydown = function checkKeycode(e){
+                if((e.keyCode == 13) && e.ctrlKey) 
+                return startComment();     
+}
+               
+   function startComment(){
+    var commentConteiner = document.createElement('div');
+    commentConteiner.classList.add("comment");
+    myDate();
+    startKomm();
+    userName();
+        }
+           function startKomm(){
+            var text = document.getElementById('txt').value;
+            var komm = document.createElement('p');
+            var newText = document.createTextNode(text);
+            komm.appendChild(newText);
+            document.getElementById('komments').appendChild(komm);
+            komm.classList.add("comment__message");
+            komm.classList.add("comment__add");
+
+            }
+          function userName(){
+                    var authorCom = document.createElement('p');
+                    var newUser = document.createTextNode("You");
+                    authorCom.appendChild(newUser);
+                    document.getElementById('authorComment').appendChild(authorCom);
+                    authorCom.classList.add("comment__user__name");   
+                }
+            function myDate(){
+                    var today = new Date;
+                    var curr_date = today.getDate();
+                    var curr_month = today.getMonth() + 1;
+                    var curr_year = today.getFullYear();
+                    var myDate = document.createElement('p');
+                    var newDate = document.createTextNode(curr_date + " " + curr_month + " " + curr_year);
+                    myDate.appendChild(newDate);
+                    document.getElementById('authorDate').appendChild(myDate);
+                    myDate.classList.add("comment__user__date"); 
+                }
+       
